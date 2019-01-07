bacfd27b871185058c8bb479b066d25bd05a39da
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-01-07 15:53:14 +0000

    Remove dist, up vers

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(join(abspath(dirname(__file__)), 'README.md'), encoding='utf-8') as re
           # Versions should comply with PEP440.  For a discussion on single-sourcing
           # the version across setup.py and the project code, see
           # https://packaging.python.org/en/latest/single_source_version.html
-          version='2.0.0a1',
+          version='2.0.0',
 
           description='SQLite REPL written in python3',
 
